a40c456f1f7df3e9b58cd865219ce270ce3001fb
asishsahu946 <asishsahu946@gmail.com>
2024-12-30 19:48:11 +0530

    firebase added

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,35 @@ import Jobs from "./pages/Jobs";
 import JobDetails from "./pages/JobDetails";
 import Footer from "./components/Footer";
 
+import { collection, query, getDocs } from "firebase/firestore";
+import { db } from "./firebase.config";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [jobs, setJobs] = useState([])
+
+const fetchJobs = async() => {
+  const tempJobs = []
+  const q = query(collection(db, "jobs"))
+  const querySnapshot = await getDocs(q);
+querySnapshot.forEach((job) => {
+  // console.log(doc.id, " => ", doc.data());
+  console.log(job.data());
+  tempJobs.push({
+    ...job.data(),
+    id: job.id,
+    postedOn: job.data().postedOn.toDate() 
+  })
+});
+setJobs(tempJobs)
+}
+
+useEffect(() => {
+  fetchJobs()
+},[])
+
+ 
   return (
     <div>
       <Navbar />
